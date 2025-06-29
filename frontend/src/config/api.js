cf5264f825204aa35edb25c44f885dbe535c931d
Asmi Prasad <prasadasmi619@gmail.com>
2025-06-29 15:17:12 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true // optional: only if using cookies/sessions
+  baseURL: "https://skailamaassignment-rgnw.onrender.com",
+  withCredentials: true // include credentials if your backend needs cookies
 });
 
 api.interceptors.request.use((config) => {
